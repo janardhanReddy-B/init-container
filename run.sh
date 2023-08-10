abb3b86e10dc6a2f8a85a37eb7fa7f18815c52f3
JanardhanReddy <bommireddy246@gmail.com>
2023-08-11 00:50:51 +0100

    terraform

--- run.sh
+++ run.sh
@@ -5,15 +5,16 @@ if [ "$skip_schema" == "true" ]; then
   exit
 fi
 
-git clone https://github.com/janardhanReddy-B/$component
-cd $component/schema
-
 if [ "$dbtype" == "mysql" ]; then
+  git clone https://github.com/janardhanReddy-B/shipping
+  cd shipping/schema
   echo "GRANT ALL ON cities.* TO 'shipping'@'%' IDENTIFIED BY 'secret';" | mysql -h $dbhost -u$dbuser -p$dbpass
-  mysql -h $dbhost -u$dbuser -p$dbpass <$component.sql
+  mysql -h $dbhost -u$dbuser -p$dbpass <shipping.sql
 fi
 
 if [ "$dbtype" == "mongo" ]; then
+  git clone https://github.com/janardhanReddy-B/$component
+  cd $component/schema
   curl -L -O https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem
   mongo --ssl --host $dbhost:27017 --sslCAFile /app/rds-combined-ca-bundle.pem --username $dbuser --password $dbpass < $component.js
 fi
