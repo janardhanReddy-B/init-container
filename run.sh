f3cbfe2983fe1ae4dde63e1c59ff69ee87b9fef7
JanardhanReddy <bommireddy246@gmail.com>
2023-08-11 01:39:12 +0100

    terraform

--- run.sh
+++ run.sh
@@ -8,13 +8,13 @@ fi
 if [ "$dbtype" == "mysql" ]; then
   git clone https://github.com/janardhanReddy-B/shipping
   cd shipping/schema
-  echo "GRANT ALL ON cities.* TO 'shipping'@'%' IDENTIFIED BY 'secret';" | mysql -h prod-rds.cluster-cajpnbnycbmh.us-east-1.rds.amazonaws.com -u$dbuser -p$dbpass
-  mysql -h prod-rds.cluster-cajpnbnycbmh.us-east-1.rds.amazonaws.com -u$dbuser -p$dbpass <shipping.sql
+  echo "GRANT ALL ON cities.* TO 'shipping'@'%' IDENTIFIED BY 'secret';" | mysql -h prod-rds.cluster-cajpnbnycbmh.us-east-1.rds.amazonaws.com -uroboshop -proboshop123
+  mysql -h prod-rds.cluster-cajpnbnycbmh.us-east-1.rds.amazonaws.com -uroboshop -proboshop123 <shipping.sql
 fi
 
 if [ "$dbtype" == "mongo" ]; then
   git clone https://github.com/janardhanReddy-B/$component
   cd $component/schema
   curl -L -O https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
-  mongo --ssl --host prod-docdb.cluster-cajpnbnycbmh.us-east-1.docdb.amazonaws.com:27017 --sslCAFile global-bundle.pem --username $dbuser --password $dbpass < $component.js
+  mongo --ssl --host prod-docdb.cluster-cajpnbnycbmh.us-east-1.docdb.amazonaws.com:27017 --sslCAFile global-bundle.pem --username roboshop --password roboshop123 < $component.js
 fi
